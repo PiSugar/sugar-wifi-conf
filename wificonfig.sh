569a0e4eafc601f588d97393e613b3724bdc4e4c
铲屎将军 <37292630+wlmh110@users.noreply.github.com>
2019-03-23 13:08:42 +0800

    Create wificonfig.sh

---
+++ wificonfig.sh
@@ -0,0 +1,6 @@
+#/bin/bash
+cd sugar-wifi-conf/build
+sudo chmod 777 binding.node
+sudo chmod 777 sugar-wifi-conf
+sudo sed -i '/sugar-wifi-conf/d' /etc/rc.local
+sudo sed -i '/exit 0/i sudo /home/pi/sugar-wifi-conf/sugar-wifi-conf' /etc/rc.local
