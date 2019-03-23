e38788cdc2bb65305c77d547ea4680cd83f224a0
铲屎将军 <37292630+wlmh110@users.noreply.github.com>
2019-03-23 22:26:39 +0800

    Update wificonfig.sh

--- wificonfig.sh
+++ wificonfig.sh
@@ -1,6 +1,10 @@
 #/bin/bash
+echo -e "Set execution permissions..."
 cd sugar-wifi-conf/build
 sudo chmod 777 binding.node
 sudo chmod 777 sugar-wifi-conf
+echo -e "Add to startup..."
 sudo sed -i '/sugar-wifi-conf/d' /etc/rc.local
 sudo sed -i '/exit 0/i sudo /home/pi/sugar-wifi-conf/build/sugar-wifi-conf' /etc/rc.local
+echo -e "Well done Pi Star people!"
+echo -e "Please restart your raspberry pi and enjoy it!!"
