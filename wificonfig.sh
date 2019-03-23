79468131c71f0d28689fd04c2f69709274a5b36b
铲屎将军 <37292630+wlmh110@users.noreply.github.com>
2019-03-23 13:14:26 +0800

    Update wificonfig.sh

--- wificonfig.sh
+++ wificonfig.sh
@@ -3,4 +3,4 @@ cd sugar-wifi-conf/build
 sudo chmod 777 binding.node
 sudo chmod 777 sugar-wifi-conf
 sudo sed -i '/sugar-wifi-conf/d' /etc/rc.local
-sudo sed -i '/exit 0/i sudo /home/pi/sugar-wifi-conf/sugar-wifi-conf' /etc/rc.local
+sudo sed -i '/exit 0/i sudo /home/pi/sugar-wifi-conf/build/sugar-wifi-conf' /etc/rc.local
