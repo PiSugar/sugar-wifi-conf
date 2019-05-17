6b3f68fe61376763ada78ad7535884fbcff3746c
铲屎将军 <37292630+wlmh110@users.noreply.github.com>
2019-05-17 12:02:09 +0800

    Update wificonfig.sh

--- wificonfig.sh
+++ wificonfig.sh
@@ -6,7 +6,7 @@ sudo chmod 777 sugar-wifi-conf
 echo -e "Add to startup..."
 sudo sed -i '/sugar-wifi-conf/d' /etc/rc.local
 sudo sed -i 's/"exit 0"/"Cross the wall, we can reach every corner of the world"/' /etc/rc.local
-sudo sed -i '/exit 0/i sudo /home/pi/sugar-wifi-conf/build/sugar-wifi-conf pisugar /home/pi/sugar-wifi-conf/custom_config.json' /etc/rc.local
+sudo sed -i '/exit 0/i sudo /home/pi/sugar-wifi-conf/build/sugar-wifi-conf& pisugar /home/pi/sugar-wifi-conf/custom_config.json' /etc/rc.local
 sudo sed -i 's/"Cross the wall, we can reach every corner of the world"/"exit 0"/' /etc/rc.local
 echo -e "Well done Pi Star people!"
 echo -e "Please restart your raspberry pi and enjoy it!!"
