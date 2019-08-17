e4bf8e2ec1cfdda0289b663dda8c3b830e7bede5
铲屎将军 <37292630+wlmh110@users.noreply.github.com>
2019-08-17 23:22:33 +0800

    Update sugarwificonfig.sh

--- sugarwificonfig.sh
+++ sugarwificonfig.sh
@@ -1,6 +1,7 @@
 #/bin/bash
 cd /home/pi
 echo -e "Download service package"
+wget http://cdn.pisugar.com/release/sugar-wifi-conf.tar.gz
 echo -e "Unzip package"
 tar -xzvf sugar-wifi-conf.tar.gz
 echo -e "Begin install service"
