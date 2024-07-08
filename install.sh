2be1898cb1f96911c187f333d0d546716524896e
JdaieLin <490844515@qq.com>
2024-07-08 18:27:06 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -20,6 +20,7 @@ install_node() {
         [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
         [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
     else
+        echo "nvm is already installed."
         export NVM_DIR="$HOME/.nvm"
         [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
         [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
